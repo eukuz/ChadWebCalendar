72d633fabf5d21303506c179b551c9b999a3bebd
Iskander Gainullin <gajnullin.iskander@mail.ru>
2021-10-17 23:41:57 +0300

    Start Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
