b949ba8e89f82e87be7a32c061d984a6a182c1a1
eukuz <evgn.worker@gmail.com>
2021-10-24 17:09:16 +0300

    Added usage of identity in adding projects

--- ChadCalendar/ChadCalendar/Controllers/ProjectController.cs
+++ ChadCalendar/ChadCalendar/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
 ﻿using ChadCalendar.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,25 +10,28 @@ namespace ChadCalendar.Controllers
 {
     public class ProjectController : Controller
     {
+       
         public IActionResult Index()
         {
             return View();
         }
+        [Authorize]
         public IActionResult AddProject()
         {
             return View();
         }
+        [Authorize]
         [HttpPost]
         public IActionResult AddProject(Models.Project project)
         {
             using (var db = new ApplicationContext())
             {
-
-                project.User = db.Users.FirstOrDefault(); //Add identity use
+                project.User = db.Users.FirstOrDefault(u => u.Login == User.Identity.Name);
                 db.Add(project);
                 db.SaveChanges();
             }
             return View();
+            
         }
     }
 }
