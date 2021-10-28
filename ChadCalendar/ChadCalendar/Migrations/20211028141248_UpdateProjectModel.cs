9508ae75820d7cac648aaab0b314af11b9d62b8e
3toalmaz <wesdorombo@gmail.com>
2021-10-28 07:20:17 -0700

    Добавил NRepetitions, добавил иконки выпадающим списком (но через html code), поэтому в бд изменил тип данных поля IconNumber на string (было int?)

---
+++ ChadCalendar/ChadCalendar/Migrations/20211028141248_UpdateProjectModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChadCalendar.Migrations
+{
+    public partial class UpdateProjectModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IconNumber",
+                table: "Projects",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IconNumber",
+                table: "Projects",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
