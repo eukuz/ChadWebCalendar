cb9c977f41692e94dba4fc2e3b82808e05d07729
AlmazDefourten <almaz.sahabiev@outlook.com>
2021-11-13 22:08:15 +0300

    started work with tasks

--- ChadCalendar/BlazorChadCalendar/Data/Services/TaskService.cs
+++ ChadCalendar/BlazorChadCalendar/Data/Services/TaskService.cs
@@ -7,5 +7,21 @@ namespace BlazorChadCalendar.Data.Services
 {
     public class TaskService
     {
+        ApplicationContext db = new ApplicationContext();
+        public IEnumerable<Project> GetProjects(User user)
+        {
+            return db.Projects.Where(proj => proj.User == user);
+        }
+        public IEnumerable<Data.Task> GetTasks(User user)
+        {
+            return db.Tasks.Where(task => task.User == user);
+        }
+        public Data.Task GetPredecessor(int? id)
+        {
+            if (id != null)
+                return db.Tasks.FirstOrDefault(t => t.Id == id);
+            else
+                return null;
+        }
     }
 }
