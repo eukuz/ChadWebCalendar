314fde8c214afac5ee46f5b251eda930ea89b396
AlmazDefourten <almaz.sahabiev@outlook.com>
2021-11-18 09:35:28 +0300

    validation added to task add

--- ChadCalendar/BlazorChadCalendar/Data/Services/TaskService.cs
+++ ChadCalendar/BlazorChadCalendar/Data/Services/TaskService.cs
@@ -9,6 +9,13 @@ namespace BlazorChadCalendar.Data.Services
     public class TaskService
     {
         ApplicationContext db = new ApplicationContext();
+        public bool IsCorrect(in Data.Task task)
+        {
+            if (task.Name != null && task.TimeTakes != null)
+                return true;
+            else
+                return false;
+        }
         private void removeDependencies(Data.Task task)
         {
             var PredecessorDependecies = db.Tasks.Where(t => t.Predecessor == task);
@@ -46,7 +53,7 @@ namespace BlazorChadCalendar.Data.Services
         {
             return db.Users.FirstOrDefault(u => u.Login == "defourtend"/*User.Identity.Name*/);
         }
-        public void AddTask(Data.Task task, int? projectId)
+        public bool AddTask(Data.Task task, int? projectId)
         {
             User user = db.Users.FirstOrDefault(u => u.Login == "defourtend"/*User.Identity.Name*/);
             task.User = user;
@@ -55,8 +62,13 @@ namespace BlazorChadCalendar.Data.Services
             task.NRepetitions = 1;
             task.Predecessor = GetPredecessor(91);
             task.Project = db.Projects.FirstOrDefault(p => p.Id == projectId); // это странное выражение нужно потому что в модели передается только Id
-            db.Add(task);
-            db.SaveChanges();
+            if (IsCorrect(task))
+            {
+                db.Add(task);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
         }
         public async void Edit(Data.Task task, int projectId)
         {
