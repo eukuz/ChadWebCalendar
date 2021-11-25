380fcf9894c7b4a36ac58fe4a256456d957a8db6
eukuz <evgn.worker@gmail.com>
2021-11-25 21:22:36 +0300

    fix

--- ChadCalendar/ChadWebCalendar/Data/Models/Appointment.cs
+++ ChadCalendar/ChadWebCalendar/Data/Models/Appointment.cs
@@ -5,8 +5,8 @@ namespace ChadWebCalendar.Data
     public class Appointment
     {
         public string Text { get; set; }
-        public DateTime? Start { get; set; }
-        public DateTime? End { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
         public string Description { get; set; }
         public string Frequency { get; set; }
         public int RemindNMinutesBefore { get; set; }
