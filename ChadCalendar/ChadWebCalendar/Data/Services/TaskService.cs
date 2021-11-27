2f5e873b968b2734b226bbe794abc99a7ea6faf8
AlmazDefourten <almaz.sahabiev@outlook.com>
2021-11-27 20:24:36 +0300

    validation fix one

--- ChadCalendar/ChadWebCalendar/Data/Services/TaskService.cs
+++ ChadCalendar/ChadWebCalendar/Data/Services/TaskService.cs
@@ -61,6 +61,10 @@ namespace ChadWebCalendar.Data.Services
             task.NRepetitions = 1;
             task.Predecessor = GetPredecessor(91);
             task.Project = db.Projects.FirstOrDefault(p => p.Id == projectId);
+            if (String.IsNullOrWhiteSpace(task.Name))
+            {
+                task.Name = "(Нет заголовка)";
+            }
             if (IsCorrect(task))
             {
                 db.Add(task);
