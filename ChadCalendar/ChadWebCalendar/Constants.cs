f8c428ae9f633ccecf00f65c64ded3b11f420210
AlmazDefourten <almaz.sahabiev@outlook.com>
2021-11-28 20:07:59 +0300

    constants comments

--- ChadCalendar/ChadWebCalendar/Constants.cs
+++ ChadCalendar/ChadWebCalendar/Constants.cs
@@ -7,6 +7,7 @@ namespace ChadWebCalendar.Data.Services
 {
     public static class Constants
     {
+        // constants for NotificationsService
         public const int MillisecondsForSleepAfterWorkerIteration = 5000;
         public const int AdditionInMinutesForMinDT = 35;
         public const int MinutesBeforeForInterval = -1;
@@ -14,5 +15,8 @@ namespace ChadWebCalendar.Data.Services
         public const int NumberOfInitialTimePosition = 11;
         public const int LenForFullDT = 4;
         public const int LenForTrimmedDT = 5;
+        //
+
+        //здесь другие константы
     }
 }
